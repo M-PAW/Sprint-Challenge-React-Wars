7c7fa8bc7f6910f2bf095938ad1aa15a9e5b3135
Michael Walter <Michael_Patrick.Walter@outlook.com>
2020-01-24 10:54:41 -0800

    Completed MVP

---
+++ starwars/src/card/cardStyles.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+ 
+export const Cardbox = styled.div `
+    display: inline-block;
+    background-color: black;
+    color: white;
+    padding: 5px;
+    margin: 5px;
+    width: 30%;
+    opacity: 45%;
+
+`
+
+export const Header = styled.h2 `
+
+`
